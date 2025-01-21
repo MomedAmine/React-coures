afa9dea17f4a735bc9c7b3058d3b5f79bd513f76
MomedAmine <168415520+MomedAmine@users.noreply.github.com>
2025-01-21 13:14:26 +0100

    Update ReactCard.jsx

--- src/ReactCard.jsx
+++ src/ReactCard.jsx
@@ -1,9 +1,16 @@
 function RC(props) {
+    const [isActive, setActive] = useState(false);
+
+    const toggleClass = () => {
+    setActive(!isActive);
+  };
 return(
     <div className="RC">
         <h2>{props.RCName}</h2>
         <hr/>
-        {props.Rcomp}
+        <div className ={isActive ? 'hideDiv': null}>
+            {props.Rcomp}
+        </div>
     </div>
 )
 };
